b72ebe971a0e5b957ee3d4c145a2f2ecdce3e59f
IbrahimVK <ib@logrythmic.com>
2023-06-11 16:13:40 +0530

    first commit

---
+++ src/components/AnecdoteList.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addVote } from "../reducers/anecdoteReducer";
+
+function AnecdoteList() {
+  const anecdotes = useSelector((state) =>
+    state.anecdotes.sort((a, b) => b.votes - a.votes)
+  );
+  const filteredText = useSelector((state) => state.filter);
+
+  const filteredAnecdotes = anecdotes.filter((item) => {
+    return item.content.includes(filteredText);
+  });
+
+  console.log(filteredAnecdotes);
+
+  const dispatch = useDispatch();
+  const vote = (id) => {
+    dispatch(addVote(id));
+  };
+  return (
+    <>
+      {filteredAnecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default AnecdoteList;
